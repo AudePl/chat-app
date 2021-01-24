4a68e5237e5089a7374d5760f5e72eaef3b92c16
AudeP <aude.plana@gmail.com>
2021-01-24 18:32:59 +0100

    contact list done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './components/Contact';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact avatar="https://randomuser.me/api/portraits/women/17.jpg" name="Darlene MayMay"  online/>
-      <Contact name="Annie Porter" avatar="https://randomuser.me/api/portraits/women/40.jpg"/>
-      <Contact name="Jimmie Ortiz" avatar="https://randomuser.me/api/portraits/men/96.jpg" online/>
+     <ContactList/>
     </div>
   );
 }
